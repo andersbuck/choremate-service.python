4853a1f9658fbef7a5fc3056619cb80a6e935839
andersbuck <buckles.anderson@gmail.com>
2021-02-09 20:39:03 -0500

    Add initial flask chores impl

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 """Flask App Project."""
 
 from flask import Flask, jsonify
+from flask import render_template
+from markupsafe import escape
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
+@app.route('/<name>')
+def index(name=None):
+    return render_template('index.html', name=escape(name))
+
+@app.route('/chores')
+def chores():
+    json_data = {'Hello': 'Chores!'}
+    return jsonify(json_data)
+
+@app.route('/chores/<int:id>')
+def chore(id):
+    json_data = {'Hello': 'Chore %d!' % id}
     return jsonify(json_data)
 
 
