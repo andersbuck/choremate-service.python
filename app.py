76ae3042dd1e5267cf567d63331d4ea10fe5e108
andersbuck <buckles.anderson@gmail.com>
2021-02-09 21:56:40 -0500

    Add database connection and attempt read

--- app.py
+++ app.py
@@ -3,8 +3,14 @@
 from flask import Flask, jsonify
 from flask import render_template
 from markupsafe import escape
+import os
+import psycopg2
+# import dj_database_url
 app = Flask(__name__)
 
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 @app.route('/')
 @app.route('/<name>')
@@ -13,7 +19,29 @@ def index(name=None):
 
 @app.route('/chores')
 def chores():
-    json_data = {'Hello': 'Chores!'}
+    json_data = []
+    try:
+        cursor = conn.cursor()
+        postgreSQL_select_Query = "select * from choremate.chores"
+
+        cursor.execute(postgreSQL_select_Query)
+        print("Selecting rows from chores table using cursor.fetchall")
+        chores_records = cursor.fetchall() 
+        
+        print("Print each row and it's columns values")
+        for row in chores_records:
+            json_data.append({'Id':row[0],'Name':row[1],'Description':row[2],'Score':row[3]})
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        #closing database connection.
+        if(conn):
+            cursor.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
+
     return jsonify(json_data)
 
 @app.route('/chores/<int:id>')
