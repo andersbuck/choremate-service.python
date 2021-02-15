91a8409cda6e8e29860e11845d77c94a38887103
andersbuck <buckles.anderson@gmail.com>
2021-02-15 16:36:04 -0500

    Add initial Auth0 impl

--- app.py
+++ app.py
@@ -5,14 +5,84 @@ from flask import render_template
 from markupsafe import escape
 import os
 import psycopg2
+
+from functools import wraps
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+
 app = Flask(__name__)
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=os.environ['AUTH0_CLIENT_ID'],
+    client_secret=os.environ['AUTH0_CLIENT_SECRET'],
+    api_base_url=os.environ['AUTH0_API_BASE_URL'],
+    access_token_url=os.environ['AUTH0_API_BASE_URL'] + '/oauth/token',
+    authorize_url=os.environ['AUTH0_API_BASE_URL'] + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-@app.route('/')
-@app.route('/<name>')
-def index(name=None):
-    return render_template('index.html', name=escape(name))
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/')
+    return f(*args, **kwargs)
+
+  return decorated
+
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/Anderson')
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri='https://choremate-app.herokuapp.com/callback')
+
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('home', _external=True), 'client_id': os.environ['AUTH0_CLIENT_ID']}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+@app.route('/dashboard')
+@requires_auth
+def index():
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
 
 @app.route('/chores')
 def chores():
