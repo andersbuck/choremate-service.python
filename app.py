2cc12ceda474748d327c70686e3ae7823eab4c2c
andersbuck <buckles.anderson@gmail.com>
2021-02-15 22:17:04 -0500

    Add audience

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def callback_handling():
 
 @app.route('/login')
 def login():
-    return auth0.authorize_redirect(redirect_uri=APP_BASE_URL + '/callback')
+    return auth0.authorize_redirect(redirect_uri=APP_BASE_URL + '/callback', audience='https://choremate-app.herokuapp.com/')
 
 @app.route('/')
 def home():
