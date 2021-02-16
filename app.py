e97d11b9b38d141b6ad836a6952ab04a6ff2ffc0
andersbuck <buckles.anderson@gmail.com>
2021-02-15 21:59:54 -0500

    Fix case

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class AuthError(Exception):
         self.error = error
         self.status_code = status_code
 
-@APP.errorhandler(AuthError)
+@app.errorhandler(AuthError)
 def handle_auth_error(ex):
     response = jsonify(ex.error)
     response.status_code = ex.status_code
